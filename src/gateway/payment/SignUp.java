aea1d18b1852856c91f1a8e6131cc268cdd7a7c9
Keerttna <127674493+Keerttna@users.noreply.github.com>
2023-09-13 15:28:39 +0530

    SignUp Page 1

--- src/gateway/payment/SignUp.java
+++ src/gateway/payment/SignUp.java
@@ -4,12 +4,16 @@ import com.toedter.calendar.JDateChooser;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class SignUp extends JFrame {
+
+public class SignUp extends JFrame implements ActionListener {
     
-    JTextField nameField, mailField;
+    JTextField nameField, mailField, aadharField, panField, phoneField;
     JDateChooser dobField;
     JRadioButton male, female;
+    JButton loginBt, continueBt;
 
     SignUp() {
         setTitle("Application Form");
@@ -27,23 +31,17 @@ public class SignUp extends JFrame {
         add(iconLabel);
 
         //Title
-        JLabel title = new JLabel("APPLICATION FORM ");
+        JLabel title = new JLabel("Sign Up ");
         title.setForeground(Color.BLACK);
-        title.setFont(new Font("Georgia", Font.BOLD, 22));
-        title.setBounds(200,100,400,40);
+        title.setFont(new Font("Georgia", Font.PLAIN, 22));
+        title.setBounds(290,100,400,40);
         add(title);
 
-        // Page no.1
-        JLabel page = new JLabel("Page 1 ");
-        page.setForeground(Color.BLACK);
-        page.setFont(new Font("Georgia", Font.BOLD, 20));
-        page.setBounds(300,130,600,30);
-        add(page);
 
         JLabel details = new JLabel("Personal Details ");
         details.setForeground(Color.BLACK);
-        details.setFont(new Font("Garamond", Font.BOLD, 20));
-        details.setBounds(245,160,600,30);
+        details.setFont(new Font("Georgia", Font.PLAIN, 22));
+        details.setBounds(255,150,600,30);
         add(details);
         
         //Name
@@ -93,7 +91,7 @@ public class SignUp extends JFrame {
         genderGroup.add(male);
         genderGroup.add(female);
 
-        //Name
+        //Mail
         JLabel mail = new JLabel("E-Mail: ");
         mail.setForeground(Color.BLACK);
         mail.setFont(new Font("Garamond", Font.BOLD, 20));
@@ -105,20 +103,72 @@ public class SignUp extends JFrame {
         mailField.setFont(new Font("Georgia", Font.PLAIN, 20));
         add(mailField);
 
+        //Aadhar No
+        JLabel aadharNo = new JLabel("Aadhar No: ");
+        aadharNo.setForeground(Color.BLACK);
+        aadharNo.setFont(new Font("Garamond", Font.BOLD, 20));
+        aadharNo.setBounds(100,360,150,30);
+        add(aadharNo);
 
+        aadharField = new JTextField(15);
+        aadharField.setBounds(270, 360, 300, 30);
+        aadharField.setFont(new Font("Georgia", Font.PLAIN, 20));
+        add(aadharField);
 
+        //PAN No
+        JLabel panNo = new JLabel("PAN No: ");
+        panNo.setForeground(Color.BLACK);
+        panNo.setFont(new Font("Garamond", Font.BOLD, 20));
+        panNo.setBounds(100,400,150,30);
+        add(panNo);
 
+        panField = new JTextField(15);
+        panField.setBounds(270, 400, 300, 30);
+        panField.setFont(new Font("Georgia", Font.PLAIN, 20));
+        add(panField);
 
+        //Phone No
+        JLabel phoneNo = new JLabel("Phone No.");
+        phoneNo.setForeground(Color.BLACK);
+        phoneNo.setFont(new Font("Garamond", Font.BOLD, 20));
+        phoneNo.setBounds(100,440,150,30);
+        add(phoneNo);
 
+        JLabel info = new JLabel("(To be linked with the account)");
+        info.setForeground(Color.DARK_GRAY);
+        info.setFont(new Font("Garamond", Font.BOLD, 18));
+        info.setBounds(40,460,300,30);
+        add(info);
 
+        phoneField = new JTextField(15);
+        phoneField.setBounds(270, 440, 300, 30);
+        phoneField.setFont(new Font("Georgia", Font.PLAIN, 20));
+        add(phoneField);
 
+        //Member info
+        JLabel accountInfo = new JLabel("Already have an account?");
+        accountInfo.setForeground(Color.BLACK);
+        accountInfo.setFont(new Font("Garamond", Font.BOLD, 18));
+        accountInfo.setBounds(40,510,300,30);
+        add(accountInfo);
 
+        //Login button
+        loginBt = new JButton("Login");
+        loginBt.setFont(new Font("Georgia", Font.PLAIN, 20));
+        loginBt.setForeground(Color.BLACK);
+        loginBt.setBackground(new Color(184,241,251));
+        loginBt.setBounds(250, 510, 100, 30);
+        loginBt.addActionListener(this);
+        add(loginBt);
 
-
-
-
-
-
+        //Continue button
+        continueBt = new JButton("Continue");
+        continueBt.setFont(new Font("Georgia", Font.PLAIN, 20));
+        continueBt.setForeground(Color.BLACK);
+        continueBt.setBackground(new Color(125, 215, 161));
+        continueBt.setBounds(450, 510, 130, 30);
+        continueBt.addActionListener(this);
+        add(continueBt);
 
         //Add background
         ImageIcon bgImgIcon = new ImageIcon(ClassLoader.getSystemResource("SignUpBg.jpg"));
@@ -133,10 +183,17 @@ public class SignUp extends JFrame {
 
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
 
 
     public static void main(String[] args) {
         new SignUp();
 
     }
+
+
 }
